6c0b8d7ea0f66bc857604de19b3c6c960e205651
jem1331 <105052745+jem1331@users.noreply.github.com>
2022-06-22 12:44:52 +0500

    Create classwork.java

---
+++ first_program/classwork.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = x;
+    x = x + 1;
+
+    final String MY_GOOD_CAR = "Filipp";
+    String text = "Filipp";
+    
+    System.out.println ("Hello, " + MY_GOOD_CAR + "!!");
+  }
+}
